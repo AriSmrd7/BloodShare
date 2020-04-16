15c74eb65698bf22278079e202481455c8e0d4eb
Ari Sumardi <arismrd13@gmail.com>
2020-04-16 21:12:45 +0700

    Cicil lagi

---
+++ app/src/main/java/com/arismrd/bloodshare/Almost.kt
@@ -0,0 +1,12 @@
+package com.arismrd.bloodshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Almost : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_almost)
+    }
+}
