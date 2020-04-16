655cccc0f20dc174fcf51e48fd4188f1dca5dc82
Ari Sumardi <arismrd13@gmail.com>
2020-04-16 10:39:31 +0700

    Cicil dulu

--- app/src/main/java/com/arismrd/bloodshare/MainActivity.kt
+++ app/src/main/java/com/arismrd/bloodshare/MainActivity.kt
@@ -2,11 +2,20 @@ package com.arismrd.bloodshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var register: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        register = findViewById(R.id.textregister)
+        register.setOnClickListener {
+            startActivity(Intent(this, Register::class.java))
+        }
     }
 }
